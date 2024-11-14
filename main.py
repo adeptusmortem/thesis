3997f7fd9be455612bae2e46027c7dfd77788034
IASelivanov <IASelivanov@mai.education>
2024-11-14 13:14:54 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from video_process_unit import VideoProcessor
 
-Processor = VideoProcessor(save_data = False)
+MainProcessor = VideoProcessor(save_data = False)
 # Processor.analyze_video()
 # Processor.analyze_video('data/video/video1.mp4')
-Processor.analyze_img('data/photos/photo1.jpg')
+MainProcessor.analyze_img('data/photos/photo1.jpg')
 # Processor.analyze_img()
